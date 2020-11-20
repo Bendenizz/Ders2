7474c7242b6bc6e6af85a5d2fa8041b6558ba0d4
Bendenizz <atesdeniz119@gmail.com>
2020-11-20 17:51:09 +0300

    ikinci commit

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -1,5 +1,6 @@
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println("birinci komut");
+        System.out.println("deniz branchinden yazıyorum" );
     }
 }
