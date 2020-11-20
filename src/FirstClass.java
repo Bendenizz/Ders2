1976644634ce9f038521582df20f4d125f59cdcd
Bendenizz <atesdeniz119@gmail.com>
2020-11-20 18:01:57 +0300

    mesaj

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -2,5 +2,6 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("birinci komut");
         System.out.println("deniz branchinden yazıyorum" );
+        System.out.println("yeni bir deneme");
     }
 }
